a0540e1f2d7038c5bb5d0eed9abf0b151b290e7b
Waqar <urspecial1one@gmail.com>
2022-05-20 23:18:12 +0500

    Added Google Analytics

--- App.d36a57b6.js
+++ App.d36a57b6.js
@@ -38313,7 +38313,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55099" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65220" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
